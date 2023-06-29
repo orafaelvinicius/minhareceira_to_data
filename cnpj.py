be48716bda2e781953b64d3eb07c17f40ca8e173
Rafael <rafael@beanalytic.com.br>
2023-06-29 18:51:41 -0300

    [ok] codigo orientado a objetos

--- cnpj.py
+++ cnpj.py
@@ -3,63 +3,67 @@ import requests
 import pandas as pd
 import numpy as np
 
-# Faça a requisição dos dados
-def Request(cnpj):
-    response = requests.get(f'https://minhareceita.org/{cnpj}')
-    
-    json_data = json.loads(response.text)
+class CPNJ:
+    def __init__(self, cnpj) -> None:
+        self.cnpj = cnpj
 
-    return json_data
+    # Faça a requisição dos dados
+    def Request(cnpj):
+        response = requests.get(f'https://minhareceita.org/{cnpj}')
+        
+        json_data = json.loads(response.text)
 
-def structure_and_treatment(json_data):
-    data_formated = []
+        return json_data
 
-    cnpj =                   json_data['cnpj']
-    nome_fantasia =          json_data['nome_fantasia']
-    estado =                 json_data['uf']
-    cep =                    json_data['cep']
-    atividade_principal =    json_data['cnae_fiscal_descricao']
-    socios =                 json_data['qsa']
-    atividades_secundarias = json_data['cnaes_secundarios']
-    
-    data_formated.append({'cnpj':cnpj})
-    data_formated.append({'nome_fantasia':nome_fantasia})
-    data_formated.append({'estado':estado})
-    data_formated.append({'cep':cep})
-    data_formated.append({'atividade_principal':atividade_principal})
-    
-    for item in socios:
-        nome_socio = item['nome_socio']
-        data_formated.append({'socios':nome_socio})
+    def structure_and_treatment(json_data):
+        data_formated = []
 
-    for atividade in atividades_secundarias:
-        atividades_secundarias = atividade['descricao']
-        data_formated.append({'atividade_secundaria':atividades_secundarias})
-    
-    return data_formated
+        cnpj =                   json_data['cnpj']
+        nome_fantasia =          json_data['nome_fantasia']
+        estado =                 json_data['uf']
+        cep =                    json_data['cep']
+        atividade_principal =    json_data['cnae_fiscal_descricao']
+        socios =                 json_data['qsa']
+        atividades_secundarias = json_data['cnaes_secundarios']
+        
+        data_formated.append({'cnpj':cnpj})
+        data_formated.append({'nome_fantasia':nome_fantasia})
+        data_formated.append({'estado':estado})
+        data_formated.append({'cep':cep})
+        data_formated.append({'atividade_principal':atividade_principal})
+        
+        for item in socios:
+            nome_socio = item['nome_socio']
+            data_formated.append({'socios':nome_socio})
 
-def create_dataframe(data):
-    print('CRIANDO O DATAFRAME')
-    # data_json = pd.read_json(data)
+        for atividade in atividades_secundarias:
+            atividades_secundarias = atividade['descricao']
+            data_formated.append({'atividade_secundaria':atividades_secundarias})
+        
+        return data_formated
 
-    df = pd.DataFrame(data)
+    def create_dataframe(data):
+        print('CRIANDO O DATAFRAME')
+        # data_json = pd.read_json(data)
 
-    return df
+        df = pd.DataFrame(data)
 
-def create_excel(df, filename):
-    df.to_excel(filename, index=False)
+        return df
+
+    def create_excel(df, filename):
+        df.to_excel(filename, index=False)
 
 
 cnpj = '30231143000175'
 
 print('Fazendo a requisição na api')
-json_data = Request(cnpj)
+json_data = CPNJ.Request(cnpj)
 
 print('Estruturando e tratando dados')
-data = structure_and_treatment(json_data)
+data = CPNJ.structure_and_treatment(json_data)
 
 print('Criando tabela')
-df = create_dataframe(data)
+df = CPNJ.create_dataframe(data)
 
 print('Criando planilha no excel')
-create_excel(df, 'info_cnpj.xlsx')
+CPNJ.create_excel(df, 'info_cnpj.xlsx')
