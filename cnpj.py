786f60f03e291a5f01e9f521f76daf81706797b0
Rafael <rafael@beanalytic.com.br>
2023-07-02 15:47:33 -0300

    [ok] ajuste no código para melhorar a orientacao a objetis

--- cnpj.py
+++ cnpj.py
@@ -1,69 +1,66 @@
 import json
+from pprint import pprint
 import requests
 import pandas as pd
-import numpy as np
 
-class CPNJ:
-    def __init__(self, cnpj) -> None:
+class CNPJ:
+    def __init__(self, cnpj='30231143000175'):
         self.cnpj = cnpj
+        self.json_data = {}
+        self.data_formated = []
+        self.df = pd.DataFrame()
 
-    # Faça a requisição dos dados
-    def Request(cnpj):
-        response = requests.get(f'https://minhareceita.org/{cnpj}')
+    def make_request(self):
+        response = requests.get(f'https://minhareceita.org/{self.cnpj}')
         
         json_data = json.loads(response.text)
+        pprint(json_data)
+        self.json_data.update(json_data)
 
-        return json_data
+    def structure_and_treatment(self):
+        print('ESTRUTURANDO DADOS')
+        print(self.json_data)
+        cnpj =                   self.json_data['cnpj']
+        nome_fantasia =          self.json_data['nome_fantasia']
+        estado =                 self.json_data['uf']
+        cep =                    self.json_data['cep']
+        atividade_principal =    self.json_data['cnae_fiscal_descricao']
+        socios =                 self.json_data['qsa']
+        atividades_secundarias = self.json_data['cnaes_secundarios']
 
-    def structure_and_treatment(json_data):
-        data_formated = []
-
-        cnpj =                   json_data['cnpj']
-        nome_fantasia =          json_data['nome_fantasia']
-        estado =                 json_data['uf']
-        cep =                    json_data['cep']
-        atividade_principal =    json_data['cnae_fiscal_descricao']
-        socios =                 json_data['qsa']
-        atividades_secundarias = json_data['cnaes_secundarios']
-        
-        data_formated.append({'cnpj':cnpj})
-        data_formated.append({'nome_fantasia':nome_fantasia})
-        data_formated.append({'estado':estado})
-        data_formated.append({'cep':cep})
-        data_formated.append({'atividade_principal':atividade_principal})
+        self.data_formated.append({'cnpj':cnpj})
+        self.data_formated.append({'nome_fantasia':nome_fantasia})
+        self.data_formated.append({'estado':estado})
+        self.data_formated.append({'cep':cep})
+        self.data_formated.append({'atividade_principal':atividade_principal})
         
         for item in socios:
             nome_socio = item['nome_socio']
-            data_formated.append({'socios':nome_socio})
+            self.data_formated.append({'socios':nome_socio})
 
         for atividade in atividades_secundarias:
             atividades_secundarias = atividade['descricao']
-            data_formated.append({'atividade_secundaria':atividades_secundarias})
+            self.data_formated.append({'atividade_secundaria':atividades_secundarias})
         
-        return data_formated
+        # return self.data_formated
 
-    def create_dataframe(data):
-        print('CRIANDO O DATAFRAME')
-        # data_json = pd.read_json(data)
-
-        df = pd.DataFrame(data)
-
+    def create_dataframe(self):
+        df = pd.DataFrame(self.data_formated)
+        print(df)
         return df
 
-    def create_excel(df, filename):
-        df.to_excel(filename, index=False)
+    def create_excel(self, filename):
+        print('Criando arquivo excel')
+        self.df.to_excel(filename, index=False)
+
+    def run(self):
+        self.make_request()
+        self.structure_and_treatment()
+        self.create_dataframe()
+        self.create_excel('info_cnpj.xlsx')
+        print('__FINISH__')
 
 
-cnpj = '30231143000175'
-
-print('Fazendo a requisição na api')
-json_data = CPNJ.Request(cnpj)
-
-print('Estruturando e tratando dados')
-data = CPNJ.structure_and_treatment(json_data)
-
-print('Criando tabela')
-df = CPNJ.create_dataframe(data)
-
-print('Criando planilha no excel')
-CPNJ.create_excel(df, 'info_cnpj.xlsx')
+if __name__ == '__main__':
+    req = CNPJ()
+    req.run()
